c361d0e031dccdd0ba07574e2807554bd97e8412
Mohamed Naeem <98890695+mohamednaeemm@users.noreply.github.com>
2026-06-23 11:27:08 +0300

    [1773] [Time Beats: 97.02%] [Memory Beats: 13.03%] - LeetPush

--- Count-Items-Matching-a-Rule.java
+++ Count-Items-Matching-a-Rule.java
@@ -1,25 +1,21 @@
 1class Solution {
 2    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-3        int cnt = 0;
-4        for(int i = 0; i < items.size(); i++) {
-5            if (ruleKey.equals("type")) {
-6                if (ruleValue.equals(items.get(i).get(0))) {
-7                    cnt += 1;
-8                }
-9            }
-10
-11            if (ruleKey.equals("color")) {
-12                if (ruleValue.equals(items.get(i).get(1))) {
-13                    cnt += 1;
-14                }
-15            }
-16
-17            if (ruleKey.equals("name")) {
-18                if (ruleValue.equals(items.get(i).get(2))) {
-19                    cnt += 1;
-20                }
-21            }
-22        }
-23        return cnt;
-24    }
-25}
+3        int index = 0;
+4        int cnt = 0;
+5
+6        if (ruleKey.equals("type")) {
+7            index = 0;
+8        } else if(ruleKey.equals("color")) {
+9            index = 1;
+10        } else {
+11            index = 2;
+12        }
+13
+14        for(List<String> item : items) {
+15            if (item.get(index).equals(ruleValue)) {
+16                cnt += 1;
+17            }
+18        }
+19        return cnt;
+20    }
+21}
