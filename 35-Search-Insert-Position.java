910da687071a4b65e2654d60a828bae693ec8203
Mohamed Naeem <98890695+mohamednaeemm@users.noreply.github.com>
2026-09-08 16:41:14 +0300

    [35] [Time Beats: 100.00%] [Memory Beats: 92.23%] - LeetPush

--- 35-Search-Insert-Position.java
+++ 35-Search-Insert-Position.java
@@ -1,11 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int l = 0, r = nums.length - 1, ans = r + 1;
+        int l = 0, r = nums.length - 1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] > target) {
                 r = mid - 1;
-                ans = mid;
             } else if (nums[mid] < target) {
                 l = mid + 1;
             } else {
@@ -13,6 +12,6 @@ class Solution {
             }
         
         }
-        return ans;
+        return l;
     }
 }
