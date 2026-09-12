292d1d38d7c899271f94d8048c946733eb2b3fe3
Mohamed Naeem <98890695+mohamednaeemm@users.noreply.github.com>
2026-09-12 13:14:39 +0300

    [34] [Time Beats: 100.00%] [Memory Beats: 14.20%] - LeetPush

--- 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] positions = new int[2];
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        return new int[]{left, right};
-    }
-
-    private int binarySearch(int[] nums, int target, boolean leftBias) {
-        int l = 0, r = nums.length - 1, i = -1;
-
-        while (l <= r) {
-            int m = (r + l) / 2; 
-            if(nums[m] < target) {
-                l = m + 1;
-            } else if (nums[m] > target) {
-                r = m - 1;
-            } else {
-                i = m;
-                if (leftBias) {
-                    r = m - 1;
+        int st = -1, end = -1;
+        for (int i = 0; i < 2; i++) {
+            int l = 0, r = nums.length - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if(nums[mid] > target) {
+                    r = mid - 1;
+                } else if (nums[mid] < target) {
+                    l = mid + 1;
                 } else {
-                    l = m + 1;
+                    if ( i == 0) {
+                        st = mid;
+                        r = mid - 1;
+                    } else {
+                        end = mid;
+                        l = mid + 1;
+                    }
+                    
                 }
             }
         }
-        return i;
+        return new int[] {st , end};
     }
 }
